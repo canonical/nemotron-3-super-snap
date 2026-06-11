be558cae9ae82c366098aa1e11df7588d282fc6a
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2026-06-11 17:32:54 +0200

    Update model download path in init-build.sh

--- .github/workflows/init-build.sh
+++ .github/workflows/init-build.sh
@@ -8,6 +8,6 @@ source .venv/bin/activate
 pip install --upgrade pip
 pip install -U huggingface_hub
 
-hf download farshidtz/NVIDIA-Nemotron-3-Super-120B-A12B-UD-Q4_K_M-5G --local-dir components/model-120b-a12b-q4-k-m-gguf
+hf download inference-snaps/NVIDIA-Nemotron-3-Super-120B-A12B-UD-Q4_K_M-5GB --local-dir components/model-120b-a12b-q4-k-m-gguf
 
 ls components/model-120b-a12b-q4-k-m-gguf
